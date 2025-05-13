705181898db29bf17406645425ea8c5dbaeb02bf
Jonghoon Kim <kim0070@gmail.com>
2025-05-13 09:10:29 +0900

    Java Features link added

--- src/main/java/net/java_school/blog/MyController.java
+++ src/main/java/net/java_school/blog/MyController.java
@@ -2,6 +2,7 @@ package net.java_school.blog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MyController {
@@ -9,6 +10,18 @@ public class MyController {
 	@GetMapping("/")
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping(value={"{chapter:java|jdbc|jsp|css-layout|jsp-pjt|spring|javascript|google-cloud}", 
+			"{chapter:java|jdbc|jsp|css-layout|jsp-pjt|spring|javascript|google-cloud}/{lecture}"})
+	public String lecture(@PathVariable("chapter") String chapter, 
+			@PathVariable(value="lecture", required=false) String lecture) {
+
+		if (lecture != null) {
+			return chapter + "/" + lecture;
+		} else {
+			return chapter + "/index";	
+		}
 	}
 	
 	@GetMapping("blog")
