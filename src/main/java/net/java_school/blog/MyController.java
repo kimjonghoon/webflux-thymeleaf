11b0f5b4d074579ef89a63e9a48eea5c7a81f584
Jonghoon Kim <kim0070@gmail.com>
2025-06-20 06:38:27 +0900

    handlers return Mono

--- src/main/java/net/java_school/blog/MyController.java
+++ src/main/java/net/java_school/blog/MyController.java
@@ -4,33 +4,41 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import reactor.core.publisher.Mono;
+import static reactor.core.publisher.Mono.just;
+
 @Controller
 public class MyController {
 
 	@GetMapping("/")
-	public String index() {
-		return "index";
+	public Mono<String> index() {
+		Mono<String> view = just("index");
+		return view;
 	}
 	
 	@GetMapping(value={"{chapter:java|jdbc|jsp|css-layout|jsp-pjt|spring|javascript|google-cloud}", 
 			"{chapter:java|jdbc|jsp|css-layout|jsp-pjt|spring|javascript|google-cloud}/{lecture}"})
-	public String lecture(@PathVariable("chapter") String chapter, 
+	public Mono<String> lecture(@PathVariable("chapter") String chapter, 
 			@PathVariable(value="lecture", required=false) String lecture) {
 
 		if (lecture != null) {
-			return chapter + "/" + lecture;
+			Mono<String> view = just(chapter + "/" + lecture);
+			return view;
 		} else {
-			return chapter + "/index";	
+			Mono<String> view = just(chapter + "/index");
+			return view;
 		}
 	}
 	
 	@GetMapping("blog")
-	public String getBlogHome() {
-		return "blog/index";
+	public Mono<String> getBlogHome() {
+		Mono<String> view = just("blog/index");
+		return view;
 	}
 
 	@GetMapping("blog/{year:\\d+}/{post}")
-	public String getPost(@PathVariable("year") Integer year, @PathVariable("post") String post) {
-		return "blog/" + year + "/" + post;
+	public Mono<String> getPost(@PathVariable("year") Integer year, @PathVariable("post") String post) {
+		Mono<String> view = just("blog/" + year + "/" + post);
+		return view;
 	}
 }
